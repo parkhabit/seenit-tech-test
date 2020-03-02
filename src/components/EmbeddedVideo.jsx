6d7762241133091b607d36af6804bdf581d2c1d1
Kitty Allen <kitty.allen@reedonline.co.uk>
2020-03-02 08:23:29 +0000

    adds show/hide functionality to embedded video

--- src/components/EmbeddedVideo.jsx
+++ src/components/EmbeddedVideo.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './EmbeddedVideo.css';
 
 /** The video in the iframe is to be shown or hidden by clicking on the heading text */
 
-const EmbeddedVideo = ({ headingText, video }) => {
-  return (
-    <>
-      <h3>{headingText}</h3>
-      <iframe
-        title={video.title}
-        src={video.src}
-        height="300px"
-        width="500px"
-      />
-    </>
-  )
+
+
+class EmbeddedVideo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {iframeHidden: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      iframeHidden: !prevState.iframeHidden
+    }));
+  }
+
+  render() {
+    const {headingText, video} = this.props;
+    const {iframeHidden} = this.state;
+    return (
+      <>
+        <h3 onClick={this.handleClick} className={'embeddedvideo-heading'}>{headingText}</h3>
+          <iframe
+          className={iframeHidden ? 'embeddedvideo-frame-hidden' : undefined}
+          title={video.title}
+          src={video.src}
+          height="300px"
+          width="500px"
+        />
+      </>
+    )
+  }
 }
 
 EmbeddedVideo.propTypes = {
--- src/components/EmbeddedVideo.test.jsx
+++ src/components/EmbeddedVideo.test.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import EmbeddedVideo from './EmbeddedVideo'
+import React from "react";
+import { mount } from "enzyme";
+import EmbeddedVideo from "./EmbeddedVideo";
 
-describe('EmbeddedVideo', () => {
-  it('Matches the snapshot', () => {
-    const props = {
-      headingText: 'Heading text',
-      video: { title: 'Video Title', src: 'www.url.com' }
-    }
+describe("EmbeddedVideo", () => {
+  const props = {
+    headingText: "Heading text",
+    video: { title: "Video Title", src: "www.url.com" }
+  };
+
+  it("Matches the snapshot", () => {
+    const wrapper = mount(<EmbeddedVideo {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("video starts off visible", () => {
     const wrapper = mount(<EmbeddedVideo {...props} />)
-    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find('iframe').length).toEqual(1);
+    expect(wrapper.find('iframe').hasClass('embeddedvideo-frame-hidden')).toBe(false);
+  });
+
+  it("changes visibility of video to hidden when first clicked", () => {
+    const wrapper = mount(<EmbeddedVideo {...props} />)
+    wrapper.find('h3').simulate('click');
+    expect(wrapper.find('iframe').hasClass('embeddedvideo-frame-hidden')).toBe(true);
+  });
+
+  it("toggles visibility of video from hidden to visible when clicked multiple times", () => {
+    const wrapper = mount(<EmbeddedVideo {...props} />)
+    wrapper.find('h3').simulate('click');
+    wrapper.find('h3').simulate('click');
+    expect(wrapper.find('iframe').hasClass('embeddedvideo-frame-hidden')).toBe(false);
   })
-})
+});
